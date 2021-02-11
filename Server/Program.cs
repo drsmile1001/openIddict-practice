e0a88eceb6f358da0f45d907dde14370f8abaaae
drsmile <drsmile.1001@gmail.com>
2021-02-11 23:02:49 +0800

    build: server 部分改到 server 資料夾下，加入sln

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace OpenIddicPractice
+namespace OpenIddicPractice.Server
 {
     public class Program
     {
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace OpenIddicPractice
+namespace OpenIddicPractice.Server
 {
     public class Startup
     {
@@ -16,6 +16,7 @@ namespace OpenIddicPractice
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
